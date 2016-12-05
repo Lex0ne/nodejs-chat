70a2114f59f1326bf8e18bc19e3f872c628ba2d7
Valeriy Morkovyn <minouts@gmail.com>
2016-12-05 22:23:27 +0200

    add app config

--- src/backend/app.js
+++ src/backend/app.js
@@ -8,30 +8,45 @@ import * as indexRoutes from './routes/index';
 import * as userRoutes from './routes/user';
 import http from 'http';
 import path from 'path';
+import config from './config/index';
 
 const app = express();
+const PORT = config.get('port');
+app.set('port', PORT);
 
-// all environments
-app.set('port', process.env.PORT || 3000);
-app.set('views', __dirname + '/views');
-app.set('view engine', 'ejs');
-app.use(express.favicon());
-app.use(express.logger('dev'));
-app.use(express.bodyParser());
-app.use(express.methodOverride());
-app.use(express.cookieParser('your secret here'));
-app.use(express.session());
-app.use(app.router);
-app.use(express.static(path.join(__dirname, 'public')));
-
-// development only
-if ('development' === app.get('env')) {
-    app.use(express.errorHandler());
-}
-
-app.get('/', indexRoutes.index);
-app.get('/users', userRoutes.list);
-
-http.createServer(app).listen(app.get('port'), () => {
-    console.log('Express server listening on port ' + app.get('port'));
+http.createServer(app).listen(PORT, () => {
+    console.log(`Express server listening on port ${PORT}`);
 });
+
+app.use((req, res, next) => {
+    return req.url === '/' ? res.end('Hello') : next();
+});
+
+app.use((req, res, next) => {
+    return req.url === '/test' ? res.end('Test') : next();
+});
+
+app.use((req, res) => {
+    return res.send(404);
+});
+
+// // all environments
+// app.set('port', process.env.PORT || 3000);
+// app.set('views', __dirname + '/views');
+// app.set('view engine', 'ejs');
+// app.use(express.favicon());
+// app.use(express.logger('dev'));
+// app.use(express.bodyParser());
+// app.use(express.methodOverride());
+// app.use(express.cookieParser('your secret here'));
+// app.use(express.session());
+// app.use(app.router);
+// app.use(express.static(path.join(__dirname, 'public')));
+//
+// // development only
+// if ('development' === app.get('env')) {
+//     app.use(express.errorHandler());
+// }
+//
+// app.get('/', indexRoutes.index);
+// app.get('/users', userRoutes.list);
